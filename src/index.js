0ec7b011e8d9c6ce29edb78190cab9363aca320f
Olivier Roche <olivier.roche@bobler.com>
2016-02-26 20:12:19 +0100

    ⬆️ Use reactDOM to render view
    react.render() is deprecated.
    Use of reactDOM.render() instead

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* @flow */
 import React from 'react' // we import naturally react
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux' // we import the provider from react-dedux
 /**
 The Provider make the store available to the components hierarchy below
@@ -8,20 +9,20 @@ import { createStore } from 'redux'
 import { counters } from './reducers'
 import App from './components/app'
 
-let initialState = {counters: [{
-    key: 'cafe',
-    title: 'cafe',
-    count: 1
-  },
-    {
-      key: 'clope',
+let initialState = {counters: [
+    { key: 'cafe',
+      title: 'cafe',
+      count: 1
+    },
+    { key: 'clope',
       title: 'clope',
       count: 2
-  }]}
+    }
+]}
 
 let store = createStore(counters, initialState, window.devToolsExtension ? window.devToolsExtension() : f => f)
 
-React.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
